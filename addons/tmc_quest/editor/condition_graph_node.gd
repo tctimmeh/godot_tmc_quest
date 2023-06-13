39ca9d4d247f8d4d4b7a00855de81e21ee416314
Tim Court <tctimmeh@gmail.com>
2023-06-12 21:29:42 -0600

    basic controls on quest and condition nodes

--- addons/tmc_quest/editor/condition_graph_node.gd
+++ addons/tmc_quest/editor/condition_graph_node.gd
@@ -3,8 +3,21 @@ extends "res://addons/tmc_quest/editor/base_graph_node.gd"
 
 var condition: QuestCondition: set = set_condition
 
+@onready var required_checkbox := %RequiredCheckbox
+@onready var always_checkbox := %AlwaysCheckbox
+
+func _process(delta):
+    required_checkbox.button_pressed = condition.required
+    always_checkbox.button_pressed = condition.always
+
 func set_condition(new_condition: QuestCondition):
     condition = new_condition
     if not condition:
         return
     title = condition.name
+
+func _on_required_checkbox_toggled(button_pressed:bool):
+    condition.required = button_pressed
+
+func _on_always_checkbox_toggled(button_pressed:bool):
+    condition.always = button_pressed
--- addons/tmc_quest/editor/quest_graph_node.gd
+++ addons/tmc_quest/editor/quest_graph_node.gd
@@ -3,8 +3,27 @@ extends "res://addons/tmc_quest/editor/base_graph_node.gd"
 
 var quest: Quest: set = set_quest
 
+@onready var required_checkbox := %RequiredCheckbox
+@onready var active_checkbox := %ActiveCheckbox
+@onready var hidden_checkbox := %HiddenCheckbox
+
+func _process(delta):
+    title = quest.name
+    required_checkbox.button_pressed = quest.required
+    active_checkbox.button_pressed = quest.active
+    hidden_checkbox.button_pressed = quest.hidden
+
 func set_quest(new_quest: Quest):
     quest = new_quest
     if not quest:
         return
     title = quest.name
+
+func _on_required_checkbox_toggled(button_pressed:bool):
+    quest.required = button_pressed
+
+func _on_active_checkbox_toggled(button_pressed:bool):
+    quest.active = button_pressed
+
+func _on_hidden_checkbox_toggled(button_pressed:bool):
+    quest.hidden = button_pressed
